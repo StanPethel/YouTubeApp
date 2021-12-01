52a1153bc86a3a452eb477db32d99447ae2a5b38
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-12-01 15:18:44 -0600

    pulled out backend api and made comments show up in browser

--- src/App.js
+++ src/App.js
@@ -8,11 +8,28 @@ import SearchBar from './components/SearchBar/Searchbar';
 function App () {
 
     const[youTubeData, setyouTubedata] = useState(null);
-    const[searchTerm, setsearchTerm] = useState("out door living")
+    const [loading,setLoading] = useState(false);
+    const [search, setSearch] = useState ("");
+    const[searchTerm, setSearchTerm] = useState("out door living");
+    const [videoId, setVideoId] = useState("PyMlV5_HRWk");
+    const [comments, setComments] = useState([]);
+    const [commentId, setCommentId] = useState([]);
+    const [replies,setReply] = useState([])
 
     useEffect(() => {
-        getyouTubeData();
+       // getyouTubeData();
+       getAllComments();
     }, [])
+
+    async function getAllComments(){
+        const response = await axios.get (`http://localhost:5000/api/comments/${videoId}`);
+        console.log(response.data);
+        setComments(response.data);
+    }
+
+    async function getAllReplies(){
+        const response =await axios.get (`http://localhost:5000/api/comments/${commentId}`);
+    }
 
     async function getyouTubeData(){
         const response = await axios.get (`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt5mU5fYM68eY2fAm67G1gEfwHRzN_D6s&maxResults=30&q=${searchTerm}&type=video`);
@@ -20,16 +37,38 @@ function App () {
         setyouTubedata(response.data);
     }
 
-    function getSearchTerm(){
+    // useEffect(() =>{
+    //     setSearchTerm(
+    //         videos.filter((video) =>
+    //         video.name.toLowerCase().includes(search.toLowerCase())
+    //         )
+    //     );
+    // },  [search,videos]);
+    //     if(loading){
+    //         return<p>Loading videos...</p>
+    // }
+    
+    function searchResults(){
+        <input type = "text" placeholder= "SearchVideos" onChange = {(e) => setSearchTerm(e.target.value)}/>
+        {setSearchTerm.map((video,idx) => (
+            <videoDetail key = {idx} {...video}/>
+        ))}
+    }
+    
         
 
     return(
         <div className = "App">
+            <ul>
+                {comments.map((comment)=> (comment.videoId===videoId)?<li>{comment.text}<ul> {comment.replies.map((reply)=> (reply.commentId===commentId)?<li>{reply.text}</li>:null)}</ul></li>:null)}
+            </ul>
+         
+
             {youTubeData &&
                 <div>
                     <h1></h1>
                     <Titlebar/>
-                    <SearchBar/>
+                    <SearchBar SearchVideos = {searchResults} />
                     <Footer/>
                 </div>
             }
